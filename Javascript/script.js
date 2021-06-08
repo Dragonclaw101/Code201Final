8fd164151c5f97b94150bc6ab42b55ba838e2127
Mason <mason.whitaker79@gmaik.com>
2021-06-08 08:57:22 -0700

    added everything i could with the amount of story complete, however code is disorganized and messy.

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,6 +1,6 @@
 'use strict';
 console.log('Hello world');
-// ORGANIZE CODE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+// ORGANIZE CODE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 /*- We want text to be displayed, and a event listener to be able to choose the reaction
 - User creates character name
 - after event listener triggered new text appears
@@ -24,18 +24,22 @@ var textArray = [
     {id:'documents', text:"You set to organizing the captain’s documents. Some of them look important, resembling maps and trade agreements. You read a little, and as you come across some you try to decipher the captain’s handwriting. As you read, you discover that he lied about being a merchant. The crew is really a pirate crew. You can hardly believe your luck! There seems to be some treasure on board somewhere, and it wouldn’t be too hard to find. You decide to search for the treasure. You sneak out of the cabin, creeping down into the hold of the ship. There are a few crew members around, snoring loudly. As you move some stuff around, you see something shiny. You make your way towards it, and discover a chalice filled with jewels. You reach for it, and a gruff voice sounds behind you. “Hey! You there! You weren’t thinkin’ of stealing my treasure, were ya?” You whirl around to face the captain, heart pounding. Words escape you at the moment, and you can think of nothing you can say to defend yourself. The captain grabs you by the arm, dragging you away from the treasure. He shoves you into a small cell, sneering as he does so. “That’ll teach ya. You’ll be stayin’ there until we find a nice island to drop ya off at.”"},
     {id:'food', text:"You wrinkle your nose as you set to cleaning up the food. It’s full of rotting mean and stale bread. Much of the food is covered in a slippery slime, and it takes a long time to pick everything up. You only manage to get through half of the pile before giving up. It’s too gross to continue. You pick yourself up, and look around the cabin. The papers are still a mess, and the clothes on the floor seem to smell even worse now. You can’t seem to put yourself in the mood to clean anything, so you lean against the cabin wall, regaining your composure. "},
     {id:'deck', text:"You exit the cabin to get a whiff of fresh air. The crew is rushing around and shouting at one another, and no one seems to notice you. You take in a large breath of the salty breeze, calming down. You feel incredibly relieved to be out of that disgusting cabin. You look around the deck, and notice that the crew seems to be acting strange. They seem to be practicing sword fighting, and some are guzzling ale. It seems strange for a merchant ship, but you try to ignore it. As you ready yourself to go back into the rancid cabin, someone shouts something indiscernable from the lookout mast. The crew leaps to their feet, some going below deck, and others grabbing weapons."},
+    {id:'clothes', text:"You begin to toss the captain’s clothes into a bin. You realize that this was an incredibly easy task when you finish in a matter of minutes. "},
+    {id:'cabin', text:"You dive back into the cabin, avoiding the gunfire that has begun. You decide it would be safer to stay indoors during… Whatever this is. As you try to focus on getting everything clean, you hear an explosion outside, and the ship turns. You try to find something to cling to, but everything is tumbling over. Water crashes through the widows, filling the cabin. The shijp is going down. You let out a whimper as water fills the cabin. It doesn’t take long for the entire cabin to fill, and even less time for darkness to overtake you."},
 ]
 var buttonArray = [
     {id:'startButtons', text1: 'Click here if you would like to be hired as a product mover.', text2:'Click here if you would like to take the position of cabin boy.'},
     {id:'cabinBoy', text1:'If you would like to start organizing the documents, click here.',text2:'To clean up the food mess, click here.',text3:'If you would like to pick up the captain’s clothes, click here.'},
     {id:'food', text1:'Click here to go out on the deck', text2:'Click here to continue cleaning'},
     {id:'deck', text1:'Click here if you would like to stay on deck', text2:'Click here if you would like to go back into the cabin'},
+    {id:'clothes', text1:'Click here if you would like to work on the documents.',text2:'Click here if you would like to work on the food mess. '}
 ]
 
 let reset = document.getElementById('resetButton');
 reset.addEventListener('click', resetFunc);
 function resetFunc() {
     resetListeners();
+    resetText();
     let target = document.getElementById('reactionText');
     target.textContent = textArray[0].text;
     let target0 = document.getElementById('option0');
@@ -124,17 +128,35 @@ function staydeck() {
     remove.removeEventListener('click', staydeck);
 }
 function cabin() {
-    alert("I'm sorry but this option is only for premium users, you can go to www.CYOA.com/premium to add a premium subscription");
-    let remove = document.getElementById('option1');
-    remove.removeEventListener('click', cabin);
+    alert("You have choosen to rub back into the cabin");
+    resetListeners();
+    resetText();
+    let target = document.getElementById('reactionText');
+    target.textContent = textArray[6].text;
+    let target2 = document.getElementById('option2');
+    target2.textContent = 'The End';
 }
 function countinueCleaing() {
-    alert('You have choosen to keep cleaning');
+    alert("I'm sorry but this option isn't avaiable at this time");
+    let remove = document.getElementById('option1');
+    remove.removeEventListener('click', countinueCleaing);6
 }
 function clothes() {
-    alert("I'm sorry but that option isn't avaible right now");
-    let remove = document.getElementById('option2');
-    remove.removeEventListener('click', clothes);
+    alert("You have choosen to clean the clothes");
+    let target = document.getElementById('reactionText');
+    let target0 = document.getElementById('option0');
+    let target1 = document.getElementById('option1');
+    let target2 = document.getElementById('option2');
+    target0.removeEventListener('click', documents);
+    target1.removeEventListener('click', food);
+    target2.removeEventListener('click', clothes);
+    target.textContent = textArray[5].text;
+    target0.textContent = buttonArray[4].text1;
+    target1.textContent = buttonArray[4].text2;
+    target2.textContent = 'Option 3'
+    
+    target0.addEventListener('click', documents);
+    target1.addEventListener('click', food);
 }
 function productMover() {
     alert("I'm sorry but that option is not avaible at this time");
